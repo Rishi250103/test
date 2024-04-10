a404bc81b9221188b1ef1884df27ddb2d28d2fe3
Rishi Soni <sonirishi958@gmail.com>
2024-04-10 16:33:57 +0530

    updated image at m1

--- m1.py
+++ m1.py
@@ -18,8 +18,7 @@ inputs=pd.DataFrame(data)
 prediction=model.predict(inputs)
 if st.button("Predict"):
     st.write("the flower is : ",prediction)
-
-if prediction=="iris-setosa":
-    st.image("C:\Users\DELL\Desktop\Class1\setosa.jpg", use_column_width=True)
+    if prediction=="Iris-setosa":
+     st.image("C:\Users\DELL\Desktop\Class1\setosa.jpg")
 
 
