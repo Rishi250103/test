194b55c67ca02a45c9fe441d16ddeca1795c7ba3
Rishi Soni <sonirishi958@gmail.com>
2024-04-10 15:11:47 +0530

    add streamlit

--- m1.py
+++ m1.py
@@ -1,2 +1,22 @@
+import pickle
+file_obj=open("iris_data.pkl","rb")
+model=pickle.load(file_obj)
+
 import streamlit as st
-st.title("hello")
+import pandas as pd
+
+
+
+st.title("Iris Data Classification")
+sepal_length=st.number_input("sepal_length")
+sepal_width=st.number_input("sepal_width")
+petal_length=st.number_input("petal_length")
+petal_width=st.number_input("petal_width")
+data={"sepal_length":[sepal_length],"sepal_width":[sepal_width]," petal_length":[ petal_length],"petal_width":[petal_width]}
+inputs=pd.DataFrame(data)
+
+prediction=model.predict(inputs)
+if st.button("Predict"):
+    st.write("the flower is : ",prediction)
+
+
